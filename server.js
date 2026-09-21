b6e8c5c66cfe5a36336f92f5092885e05470e4a0
hemglobin <fujitanoyuunosuke@gmail.com>
2026-09-21 11:33:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -47,7 +47,9 @@ const httpServer = http.createServer((req, res) => {
 
 });
 
-httpServer.listen(8080, () => {
+const PORT = process.env.PORT || 8080;
+
+httpServer.listen(PORT, "0.0.0.0", () => {
     console.log("Webサーバーを起動しました！");
 });
 
